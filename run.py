0059d9e46158051f73212f194398c9c3784ce9dd
sivavaak <siva.vaakiyan2003@gmail.com>
2026-01-19 14:42:22 -0500

    Added setup.py and run.py for simplified setup from scratch

---
+++ run.py
@@ -0,0 +1,36 @@
+import subprocess
+import sys
+from pathlib import Path
+
+def run_module(module_name, project_root):
+    """
+    Runs module using current interpreter
+    """
+    command = [
+        sys.executable,
+        "-m",
+        module_name,
+    ]
+
+    print ("> " + " ".join(command))
+    return subprocess.Popen(command, cwd=project_root)
+
+
+def main():
+
+    script_dir = Path(__file__).resolve().parent
+    project_root = script_dir
+
+    # modules
+    modules = [
+        "nexfault.gui.DSI_Serial_Monitor",
+        "nexfault.gui.UUT_Serial_Monitor",
+    ]
+
+    processes = []
+
+    for module in modules:
+        processes.append(run_module(module, project_root))
+
+if __name__ == "__main__":
+    main()
---
+++ setup/setup.py
@@ -0,0 +1,36 @@
+# Tool to run host side nexfault one. Installs dependencies if not detected.
+
+import platform
+import subprocess
+import sys
+from pathlib import Path
+
+def run(cmd):
+    subprocess.check_call(cmd, shell=True)
+
+def main():
+
+    script_dir = Path(__file__).resolve().parent
+    requirements = script_dir / "requirements.txt"
+    repo_root = script_dir.parent
+
+    print("OS:", platform.system())
+
+    # installs required python dependencies
+    run(f"{sys.executable} -m pip install --upgrade pip")
+    run(f"{sys.executable} -m pip install -r \"{requirements}\"")
+
+    # adds submodule (assumes git is installed)
+    output = subprocess.check_call(
+        ["git", "submodule", "update", "--init", "--recursive"],
+        cwd=repo_root,
+        text=True
+    )
+
+    print(output)
+
+    print("Installation complete.")
+    print("Run the tool with: python run.py")
+
+if __name__ == "__main__":
+    main()
